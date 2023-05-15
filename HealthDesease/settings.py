9ffd9b7c5e1320321f9bae6a1f50d8b349f91942
Souvik Rakshit <souvikrakshit45@gmail.com>
2023-05-15 13:21:31 +0530

    new update

--- HealthDesease/settings.py
+++ HealthDesease/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
