0891775a0dddeff72ca8ac029601359c6aa140d0
Souvik Rakshit <souvikrakshit45@gmail.com>
2023-05-17 11:28:05 +0530

    update

--- HealthDesease/settings.py
+++ HealthDesease/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'HealthDesease.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'Media/db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'Media/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'FGT57DOnghViSYJzSyC1',
+        'HOST': 'containers-us-west-203.railway.app',
+        'PORT': '7614',
+
     }
 }
-
 
 
 
